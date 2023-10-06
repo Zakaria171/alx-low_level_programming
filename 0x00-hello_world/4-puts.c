6cf16f7b76ff3625bc05ef7745c19bcc9c4d82b9
Zakaria171 <zakaria.rabih93@gmail.com>
2023-10-06 23:45:37 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 int main (void)
 {
-	puts ("Programming is like building a multilingual puzzle")
-		Return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
