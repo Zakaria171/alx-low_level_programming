316bd175900fb9419422f7a2ac33d34e192982d7
Zakaria171 <zakaria.rabih93@gmail.com>
2023-10-07 21:40:24 +0100

    putchar command

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase followed by a new line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char alphabet[26] = {abcdefghijklmnopqrstuvwxyz};
+	int i;
+
+	for (i = O ; i < 26 ; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	Return (0);
+}
