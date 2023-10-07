76546574a2c3921a4042ab6640668bf6e807ef74
Zakaria171 <zakaria.rabih93@gmail.com>
2023-10-07 21:03:51 +0100

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - prints the last digit of a randomly generated number
  * prints wether it's greater than 5, less than 6 or 0
  *
- * Return always 0
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
-		printf ("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
