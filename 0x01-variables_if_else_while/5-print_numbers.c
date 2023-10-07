1c2c2fe46446629f850512184b401d3675605ccd
Zakaria171 <zakaria.rabih93@gmail.com>
2023-10-07 22:35:22 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all digit numbers starting from 0
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
