f2a4cfeb73101c68d5bfec9eb47a7a57659f1534
Zakaria171 <zakaria.rabih93@gmail.com>
2023-10-10 18:25:25 +0100

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ * Return: Always(0)
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	char low_alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	i = 0;
+	while (i < 10)
+	{
+		for (j = 0 ; j < 26 ; j++)
+		{
+			_putchar(low_alp[j]);
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
+
