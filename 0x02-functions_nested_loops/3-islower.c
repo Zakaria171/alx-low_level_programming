92bf86145139bbfa1a0e2cd6d474fea363ecbd20
Zakaria171 <zakaria.rabih93@gmail.com>
2023-10-10 22:18:18 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if c is lowercase
+ * @c: uppercase or lowercase
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
+void times_table(void);
+int _islower(int c);
