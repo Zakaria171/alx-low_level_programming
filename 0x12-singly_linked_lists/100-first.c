64c05d8c27b8e3b1bb7a62322f8ece261ab3e2bc
Zakaria171 <zakaria.rabih93@gmail.com>
2023-11-19 20:33:54 +0100

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+/**
+ * first - prints a sentence before main
+ * Return: nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
