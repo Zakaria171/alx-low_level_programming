443296f8cf94f8e336f50f9c1c9a12e09661d3e4
Zakaria171 <zakaria.rabih93@gmail.com>
2023-11-22 16:18:56 +0100

    woof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define FOG_H
+/**
+ * struct dog - dog's informations
+ * @name: first member
+ * @age: seconde member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
